3af067f1d856be190e3cee390ce6a8d328772bad
thtyaph022 <thtyaph022@student.wethinkcode.co.za>
2023-03-18 02:49:16 +0200

    Transaction testing

--- src/main/java/za/co/tyaphile/admin/transact/Transaction.java
+++ src/main/java/za/co/tyaphile/admin/transact/Transaction.java
@@ -1,4 +1,13 @@
 package za.co.tyaphile.admin.transact;
 
 public class Transaction {
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public synchronized void setDeposit(double amount) {
+        this.balance += amount;
+    }
 }
--- src/test/java/za/co/tyaphile/admin/transact/TransactionTest.java
+++ src/test/java/za/co/tyaphile/admin/transact/TransactionTest.java
@@ -2,9 +2,30 @@ package za.co.tyaphile.admin.transact;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TransactionTest {
     @Test
     public void testDeposit() {
+        Transaction transaction = new Transaction();
+        double amount = transaction.getBalance();
+        assertEquals(0, amount);
+        transaction.setDeposit(100);
+        amount = transaction.getBalance();
+        assertEquals(100, amount);
+    }
+    @Test
+    public void testWithdrawal() {
+        Transaction transaction = new Transaction();
+        double amount = transaction.getBalance();
+        assertEquals(0, amount);
+        transaction.setDeposit(-100);
+        amount = transaction.getBalance();
+        assertEquals(-100, amount);
+    }
+
+    @Test
+    public void testTransactionHistory() {
 
     }
 }
